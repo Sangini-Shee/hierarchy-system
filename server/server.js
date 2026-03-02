65bf1e7107d4d026f7b9793afac64dbef60bfc3a
Sangini-Shee <sanginishee2005@gmail.com>
2026-03-02 20:26:29 +0530

    Added test route

--- server/server.js
+++ server/server.js
@@ -20,3 +20,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.get("/test-route", (req, res) => {
+  res.json({ message: "Test route working" });
+});
